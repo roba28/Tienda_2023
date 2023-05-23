33e365da098cc05d1494deeaef7a3073b5ed05ed
Ronny <Ronny@ADSP-13207>
2023-05-23 12:15:00 -0600

    Carga Inicial

---
+++ Tienda_2023/src/main/java/com/Tienda_2023/Tienda2023Application.java
@@ -0,0 +1,13 @@
+package com.Tienda_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda2023Application.class, args);
+	}
+
+}
